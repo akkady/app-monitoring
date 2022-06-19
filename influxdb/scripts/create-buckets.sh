50a21ba53dbd7f3a76ec03095fd7875c4f306c35
akkday <akkday43@gmail.com>
2022-06-19 17:50:37 +0100

    telegraf multiple instance (agents)

--- influxdb/scripts/create-buckets.sh
+++ influxdb/scripts/create-buckets.sh
@@ -6,4 +6,7 @@ influx bucket create -n ${DOCKER_INFLUXDB_SQL_SERVER_BUCKETS} -o ${DOCKER_INFLUX
 influx bucket create -n ${DOCKER_INFLUXDB_JMETER_BUCKETS} -o ${DOCKER_INFLUXDB_INIT_ORG} -r ${DOCKER_INFLUXDB_INIT_RETENTION}
 influx bucket create -n ${DOCKER_INFLUXDB_SOLR_BUCKETS} -o ${DOCKER_INFLUXDB_INIT_ORG} -r ${DOCKER_INFLUXDB_INIT_RETENTION}
 influx bucket create -n ${DOCKER_INFLUXDB_HYBRIS_BUCKETS} -o ${DOCKER_INFLUXDB_INIT_ORG} -r ${DOCKER_INFLUXDB_INIT_RETENTION}
+##
+# run this command in influx cli to map between bucket and a database that use InfluxQL
+#influx v1 dbrp create --db sqlserver_v1 --bucket-id [bucket id exp: 6939f114922dab04] --rp example-rp
 
