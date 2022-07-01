524a3dc02acf4d6d72ffe512608ded03652d423a
akkday <akkday43@gmail.com>
2022-07-01 07:15:08 +0100

    custom dashboards

--- influxdb/scripts/create-buckets.sh
+++ influxdb/scripts/create-buckets.sh
@@ -7,6 +7,5 @@ influx bucket create -n ${DOCKER_INFLUXDB_JMETER_BUCKETS} -o ${DOCKER_INFLUXDB_I
 influx bucket create -n ${DOCKER_INFLUXDB_SOLR_BUCKETS} -o ${DOCKER_INFLUXDB_INIT_ORG} -r ${DOCKER_INFLUXDB_INIT_RETENTION}
 influx bucket create -n ${DOCKER_INFLUXDB_HYBRIS_BUCKETS} -o ${DOCKER_INFLUXDB_INIT_ORG} -r ${DOCKER_INFLUXDB_INIT_RETENTION}
 ##
-# run this command in influx cli to map between bucket and a database that use InfluxQL
-#influx v1 dbrp create --db sqlserver_v1 --bucket-id [bucket id exp: 6939f114922dab04] --rp example-rp
-
+## run this command in influx cli to map between bucket and a database that use InfluxQL
+# influx v1 dbrp create --db sqlserver_v1 --bucket-id c2314036499577b6 --rp default --org ${DOCKER_INFLUXDB_INIT_ORG} --default -t ${DOCKER_INFLUXDB_INIT_ADMIN_TOKEN}
